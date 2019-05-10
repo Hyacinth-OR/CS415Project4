2d44826feb7409d60ea62a2438421b62cc0d0b7b
JeffOlson96 <olsonje@sonoma.edu>
2019-05-10 15:49:14 -0700

    still need to figure out memory function but hashtable should work

---
+++ 1b.py
@@ -0,0 +1,126 @@
+import sys
+import time
+import heapq
+import math
+
+
+
+def hash_helper(i,j):
+    n = len(v)
+    W = len(w)
+    bn = math.ceil(math.log(n+1, 2))
+    bw = math.ceil(math.log(W+1, 2))
+    #print("bnstr: ", bn)
+
+    #takes 0b of front of bin()
+    bn_str = bin(i)[2:]
+    bw_str = bin(j)[2:]
+
+    bn_str1 = str(bn_str.zfill(bn))
+    bw_str1 = str(bw_str.zfill(bw))
+
+    
+    r = "0b1" + bn_str1 + bw_str1
+    
+    return int(r, 2)
+
+
+# data is value of F(i,j)
+class LLNode:
+	def __init__(self, i, j, val):
+		self.i = i
+		self.j = j
+		self.val = val	
+		self.next = None
+
+
+class LinkedList:
+	def __init__(self)
+		self.head = None
+		self.cur  = None
+		self.size = 0
+
+	def add_node(self, i, j, val):
+		self.size+=1
+		new_node = Node()
+		new_node.i = i
+		new_node.j = j
+		new_node.val = val
+		if self.size == 1:
+			self.head = new_node
+			new_node.next = None
+		else:
+			new_node.next = self.cur
+			self.cur = new_node
+
+
+	def get_node(self, i, j):
+		found = False
+		node = self.cur
+		while (!found):
+			if (i == node.i && j == node.j):
+				found = True
+				return node
+			else
+				node = node.next
+		return None
+
+
+class HashEntry:
+	def __init__(self, i, j, key, val):
+		self.ll = LinkedList()
+		ll.add_node(i,j)
+		self.key = key
+		
+
+	def getKey():
+		return self.key
+
+	def Handle_Collision(i,j):
+		link = self.ll
+		link.add_node(i,j,val)
+
+	def getNode(i,j):
+		link = self.ll
+		ret_node = link.get_node(i,j)
+		return ret_node
+
+
+class HashTable:
+
+	def __init__(self, k):
+		self.size = k;
+		self.table = [None] * self.size
+
+
+
+	def HashFunc(i, j):
+		return hash_helper(i,j) % self.size
+
+	def Insert(i, j, key):
+		key = HashFunc(i,j)
+		entry = HashEntry(i,j, key)
+		
+		if (self.table[key] == None):
+			self.table[key] = entry
+		else:
+			temp = self.table[key]
+			temp.Handle_Collision(i,j)
+
+	def Search(i, j, key):
+		#found = False
+		cur = self.table[key]
+		if (cur.getNode(i,j) != None):
+			return cur
+		else
+			return None
+
+
+
+
+
+
+
+
+
+		
--- knapsack.py
+++ knapsack.py
@@ -1,13 +1,130 @@
 """CS415 Project 4 - Created By Colin Dutra and Jeff Olson Spring 2019"""
+
+#!/usr/bin/python2.7
+
 import sys
 import time
 import heapq
+import math
 import matplotlib.pyplot as plt
 DEVELOPMENT_MODE = True
 GRAPH_MODE = True
 w = [0]
 v = [0]
 F = [0]
+M = [0]
+
+
+
+def hash_helper(i,j):
+    n = len(v)
+    W = len(w)
+    bn = math.ceil(math.log(n+1, 2))
+    bw = math.ceil(math.log(W+1, 2))
+    #print("bnstr: ", bn)
+
+    #takes 0b of front of bin()
+    bn_str = bin(i)[2:]
+    bw_str = bin(j)[2:]
+
+    bn_str1 = str(bn_str.zfill(bn))
+    bw_str1 = str(bw_str.zfill(bw))
+
+    
+    r = "0b1" + bn_str1 + bw_str1
+    
+    return int(r, 2)
+
+
+# data is value of F(i,j)
+class LLNode:
+    def __init__(self, i, j, val):
+        self.i = i
+        self.j = j
+        self.val = val  
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.cur  = None
+        self.size = 0
+
+    def add_node(self, i, j, val):
+        self.size+=1
+        new_node = Node()
+        new_node.i = i
+        new_node.j = j
+        new_node.val = val
+        if self.size == 1:
+            self.head = new_node
+            new_node.next = None
+        else:
+            new_node.next = self.cur
+            self.cur = new_node
+
+
+    def get_node(self, i, j):
+        found = False
+        node = self.cur
+        while (found != True):
+            if (i == node.i and j == node.j):
+                found = True
+                return node
+            else:
+                node = node.next
+        return None
+
+
+class HashEntry:
+    def __init__(self, i, j, key):
+        self.ll = LinkedList()
+        ll.add_node(i,j)
+        self.key = key
+        
+
+    def getKey():
+        return self.key
+
+    def Handle_Collision(i,j):
+        link = self.ll
+        link.add_node(i,j)
+
+    def getNode(i,j):
+        link = self.ll
+        ret_node = link.get_node(i,j)
+        return ret_node
+
+
+class HashTable:
+
+    def __init__(self, k):
+        self.size = k;
+        self.table = [None] * self.size
+
+
+
+    def HashFunc(i, j):
+        return hash_helper(i,j) % self.size
+
+    def Insert(i, j, key):
+        key = HashFunc(i,j)
+        entry = HashEntry(i,j, key)
+        
+        if (self.table[key] == None):
+            self.table[key] = entry
+        else:
+            temp = self.table[key]
+            temp.Handle_Collision(i,j)
+
+    def Search(i, j, key):
+        #found = False
+        cur = self.table[key]
+        if (cur.getNode(i,j) != None):
+            return cur
+        else:
+            return None
 
 
 def traditional(v, w, capacity, mode = 0):
@@ -46,8 +163,59 @@ def traditional(v, w, capacity, mode = 0):
         return d[rows - 1][cols - 1], sorted(subset)
 
 
-def hashsack(i,j): # i = number if items, j = capacity
+def space_efficient(v,w,capacity):
+    global M
+    rows = len(v) + 1
+    cols = capacity + 1
+
+
+    # adding dummy values as later on we consider these values as indexed from 1 for convinence
+    v = [0] + v[:]
+    w = [0] + w[:]
+
+    
+
+    # row : values , #col : weights
+    # what Ian and thought was a solid size for k
+    sizek = math.sqrt((len(v)*len(w))/2)
+    HTable = HashTable(sizek)
+        
+    for i in range(1, rows):
+        # weights
+        for j in range(1, cols):
+            # if this weight exceeds max_weight at that point
+            if j - w[i] < 0:
+                M[i][j] = M[i - 1][j]
+
+            # max of -> last ele taken | this ele taken + max of previous values possible
+            else:
+                M[i][j] = hashsack(i,j,HTable)
+                #M[i][j] = max(M[i - 1][j], v[i] + M[i - 1][j - w[i]])
+
+    subset = []
+    i = rows - 1
+    j = cols - 1
+
+    while i > 0 and j > 0:
+        if M[i][j] != M[i - 1][j]:
+            subset.append(i)
+
+            j = j - w[i]
+            i = i - 1
+
+        else:
+            i = i - 1
+
+    return M[rows - 1][cols - 1], sorted(subset)
+
+
+
+def hashsack(i,j,HTable): # i = number if items, j = capacity
     global F
+    if i == 0:
+        return 0
+    if j == 0:
+        return 0
     if F[i][j] < 0:
         if j < w[i]:
             value = hashsack(i-1,j)
@@ -56,12 +224,30 @@ def hashsack(i,j): # i = number if items, j = capacity
         F[i][j] = value
     return F[i][j]
 
+
+def memory_func(i,j):
+    global M
+    if i == 0:
+        return 0
+    if j == 0:
+        return 0
+    elif M[i][j] < 0: #elif i,j is in the hash table
+        if j < w[i]:
+            val = memory_func(i-1, j)
+        else:
+            val = max(memory_func(i-1, j), v[i] + memory_func(i-1, j-w[i]))
+        M[i][j] = val
+    return M[i][j]
+
+
+
 def getsize(array):
     size = 0
     for elem in array:
         size+= len(elem)
 
     return size
+
 def filerr():
     print("##############################################")
     print("## File not found! Running for p01 fileset! ##")
@@ -298,7 +484,11 @@ def main():
         readout(name,value,subset,runtime)
 
 
-        name = "Space-efficient Dynamic Programming"
+        name2 = "Space-efficient Dynamic Programming"
+        start = time.perf_counter_ns()
+        value,subset = space_efficient(v,w,cap)
+        runtime = time.perf_counter_ns() - start
+        readout(name2, value, subset, runtime)
 
 
         b1name = "Greedy Approach"
